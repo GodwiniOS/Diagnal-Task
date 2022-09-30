80f0a0faf1c8204b1186daff2570f6e42d9415b1
Godwin A <iamgodwin10@gmail.com>
2022-09-30 15:08:59 +0530

    Initial Commit

---
+++ Diagnal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diagnal
+//
+//  Created by Godwin A on 30/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
